786b6601495315c825d09fb8aff0d57a83e827aa
killianfalle <killianfalle7@gmail.com>
2022-10-17 15:23:04 +0800

    fix mobile text responsiveness

--- src/sections/about/about.js
+++ src/sections/about/about.js
@@ -9,27 +9,27 @@ function About() {
                 About me
                 <PrimaryText className="text-4xl">.</PrimaryText>
             </p>
-            <p className="section-subtitle">I'm a Mobile and Web developer based in the Philippines 🇵🇭</p>
+            <p className="section-subtitle">I'm an experienced Mobile and Web developer based in the Philippines 🇵🇭</p>
             
             <div className="section-content">
-                <p className="mb-3">
+                <p>
                     For the past <b className="font-bold">4+</b> years
-                    I've experienced <b className="text-blue">Front-End</b> and <b className="text-red">Back-End</b> development.
+                    I've experienced <b className="text-secondary">Front-End</b> and <b className="text-secondary">Back-End</b> development.
                     I'm also a self-taught programmer, self-motivated and passionate for software development.
                 </p>
 
-                <p className="mb-3">
+                <p>
                     I have deployed multiple mobile applications on{" "}
                     <AppleLogoSVG className="inline -mt-0.5 md:-mt-1"/> <b>Appstore</b> and {" "}
                     <AndroidLogoSVG className="inline -mt-0.5 md:-mt-1 md:-ml-1"/><b>Playstore</b>{" "}
                     from scratch with different challenges due to strict requirements/compliance from those platforms. 
                 </p>
 
-                <p className="mb-3">
+                <p>
                     Music helps me concentrate, focus, blocks out distractions, and even makes tedious task more enjoyable.
                 </p>
 
-                <p className="mb-3">
+                <p>
                     Sometimes I go outside and take a 5-10 minute walk when I'm feeling stuck to help me return to my tasks with more refreshed and focused mind.
                 </p>
             </div>
--- src/sections/expertise/expertise.js
+++ src/sections/expertise/expertise.js
@@ -2,7 +2,7 @@ import PrimaryText from "../../components/primary-text/primary-text";
 
 function Expertise() {
     return (
-        <div className="w-full">
+        <div className="w-full flex justify-center">
             <p className="section-title">
                 Expertise
                 <PrimaryText className="text-4xl">.</PrimaryText>
