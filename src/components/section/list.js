122ee633f83550b704bbf073689953ce0814ee4b
killianfalle <killianfalle7@gmail.com>
2022-10-17 15:03:24 +0800

    added experties section

--- src/components/section/list.js
+++ src/components/section/list.js
@@ -1,10 +1,15 @@
 import About from '../../sections/about/about';
 import Contact from '../../sections/contact/contact';
+import Expertise from '../../sections/expertise/expertise';
 
 export const sectionsList = [
     {
       name: 'About',
       component: <About />
+    },
+    {
+      name: 'Expertise',
+      component: <Expertise />
     },
     {
       name: 'Contact',
