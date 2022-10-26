0a6312eac585c74430e3eb5c200b0cfb123cc56f
killianfalle <killianfalle7@gmail.com>
2022-10-26 16:39:26 +0800

    add visibility icon on expirtise grid

--- src/sections/expertise/expertise.js
+++ src/sections/expertise/expertise.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import PrimaryText from "../../components/primary-text/primary-text";
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 
 function Expertise() {
     const [state, setState] = useState({
@@ -26,16 +28,19 @@ function Expertise() {
                         <div className="bg-web grid-line"/>
                         <h2 className="grid-title">Web App Development</h2>
                         <p className="grid-description">Passionate about UI/UX and have professional experience working in HTML, CSS, JS, React and Vue frameworks.</p>
+                        {!state.showWeb ? <VisibilityOutlinedIcon className="grid-icon"/> : <VisibilityOffOutlinedIcon className="grid-icon"/>}
                     </div>
                     <div className={`grid-item mobile ${state.showMobile ? "shown" : ""}`} onClick={() => expand('showMobile')}>
                         <div className="bg-mobile grid-line"/>
                         <h2 className="grid-title">Mobile App Development</h2>
                         <p className="grid-description">Skilled in developing hybrid mobile apps and cross-platform solutions using the React Native framework.</p>
+                        {!state.showMobile ? <VisibilityOutlinedIcon className="grid-icon"/> : <VisibilityOffOutlinedIcon className="grid-icon"/>}
                     </div>
                     <div className={`grid-item backend ${state.showBackend ? "shown" : ""}`} onClick={() => expand('showBackend')}>
                         <div className="bg-backend grid-line"/>
                         <h2 className="grid-title">Backend Development</h2>
                         <p className="grid-description">Experienced developing and maintaining server-side applications. Have extensive knowledge about APIs using Laravel.</p>
+                        {!state.showBackend ? <VisibilityOutlinedIcon className="grid-icon"/> : <VisibilityOffOutlinedIcon className="grid-icon"/>}
                     </div>
                 </div>
             </div>
