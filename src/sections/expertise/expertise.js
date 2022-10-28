b1f6d8dc0775e6818641affe3768504e29be4d58
killianfalle <killianfalle7@gmail.com>
2022-10-28 22:36:03 +0800

    update experise UI/UX

--- src/sections/expertise/expertise.js
+++ src/sections/expertise/expertise.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import PrimaryText from "../../components/primary-text/primary-text";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import WebIcon from '@mui/icons-material/Web';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import StorageIcon from '@mui/icons-material/Storage';
 
-const experties = ['web', 'mobile', 'backend'];
+const expertise = ['web', 'mobile', 'backend'];
 
 function Expertise() {
     const [expand, setExpand] = useState({
@@ -17,29 +20,34 @@ function Expertise() {
         <div className="section-container centered">
              <p className="section-title">
                 <PrimaryText className="text-3xl md:text-5xl">{"{"}</PrimaryText>
-                Experties
+                Expertise
                 <PrimaryText className="text-3xl md:text-5xl">{"}"}</PrimaryText>
             </p>
 
             <div className="section-content flex justify-center items-center">
-                <div className="experties-content">
-                    {experties.map((item, index) => {
-                        return (
-                            <div
-                                key={index}
-                                className={`experties-item ${item} ${expand[item] ? "shown" : ""}`}
-                                onClick={() => handleExpand(item)}>
-                                <div className={`bg-${item} experties-line`}/>
-                                <h2 className="experties-title">{item} App Development</h2>
-                                <p className="experties-description">
-                                    {item === 'web' && "Passionate about UI/UX and have professional experience working in HTML, CSS, JS, React and Vue frameworks."}
-                                    {item === 'mobile' && "Skilled in developing hybrid mobile apps and cross-platform solutions using the React Native framework."}
-                                    {item === 'backend' && "Experienced developing and maintaining server-side applications. Have extensive knowledge about APIs using Laravel."}
-                                </p>
-                                <ArrowForwardIosIcon className="experties-icon"/>
+                <div className="expertise-content">
+                    {expertise.map((item, index) => (
+                        <div
+                            key={index}
+                            className={`expertise-item ${item} ${expand[item] ? "shown" : ""}`}
+                            onClick={() => handleExpand(item)}>
+                            {/* <div className={`expertise-line ${item}`}/> */}
+                            <div className="expertise-overview">
+                                <div className={`expertise-icon icon-${item}`}>
+                                    {item === 'web' && <span><WebIcon /></span>}
+                                    {item === 'mobile' && <span><PhoneIphoneIcon /></span>}
+                                    {item === 'backend' && <span><StorageIcon /></span>}
+                                </div>
+                                <h2 className="expertise-title">{item} App Development</h2>
                             </div>
-                        )
-                    })}
+                            <p className="expertise-description">
+                                {item === 'web' && "Passionate about UI/UX and have professional experience working in HTML, CSS, JS, React and Vue frameworks."}
+                                {item === 'mobile' && "Skilled in developing hybrid mobile apps and cross-platform solutions using the React Native framework."}
+                                {item === 'backend' && "Experienced developing and maintaining server-side applications. Have extensive knowledge about APIs using Laravel."}
+                            </p>
+                            <ArrowForwardIosIcon className="arrow-icon"/>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
