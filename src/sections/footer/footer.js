814e3230de5e89d0e8b17bd25985e42f9dc77ca9
killianfalle <killianfalle7@gmail.com>
2025-01-16 10:16:44 +0800

    update: footer message to include language used

--- src/sections/footer/footer.js
+++ src/sections/footer/footer.js
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <div className="section-container centered">
             <div className="section-footer">
-                <p>Designed & Developed by <b>Killian Falle ©️ 2022</b></p>
+                <p>Designed & Developed by <b>Killian Falle ©️ 2022</b> using React.js</p>
             </div>
         </div>
     );
