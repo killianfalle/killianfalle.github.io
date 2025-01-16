4239eb132a11d886488f423c5539eb9c630040fd
killianfalle <killianfalle7@gmail.com>
2025-01-16 10:25:49 +0800

    fix: use bold for 'react.js'

--- src/sections/footer/footer.js
+++ src/sections/footer/footer.js
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <div className="section-container centered">
             <div className="section-footer">
-                <p>Designed & Developed by <b>Killian Falle ©️ 2022</b> using React.js</p>
+                <p>Designed & Developed by <b>Killian Falle ©️ 2022</b> using <b>React.js</b></p>
             </div>
         </div>
     );
